424ee40350d9e3c6b8e5899491cbd3023987e546
Nicolas Helrigle <nicolashgneres@gmail.com>
2026-05-09 11:51:26 -0300

    Create Aula1.py

---
+++ Aula1.py
@@ -0,0 +1,13 @@
+# Exercício 1 - imprimindo imagens:)
+
+print("Bem vindos a aula de python!\nNesse curso você aprendera recursos basicos do python.\n\n")
+
+# Exercicio 2 - Formatação de saida
+
+nome = "Alice"
+idade = 25
+altura = 1.68
+
+print("Olá, meu nome é", nome,
+"e tenho", idade, "anos.")
+print(f"Eu tenho {idade} anos e minha altura é {altura:.2f} metros.")
